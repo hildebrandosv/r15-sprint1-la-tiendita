0da538f44704a8cd5e62626c36d79f034d1084fb
hildebrandosv <hildebrandosv@gmail.com>
2022-01-15 13:15:13 -0500

    Ya funciona la visualización del carrito. Sigueonerlos en el BROWSE y activar el vaciado de canasta y el botón de ir a pagar

--- tmp/tmp.js
+++ tmp/tmp.js
@@ -33,8 +33,8 @@ idUlItemsCartList.addEventListener('click', async (e) => {
   const btnSubstract = e.target.classList.contains('clBtnAddOneLess');
   const btnAdd = e.target.classList.contains('clBtnAddOneMore');
   if (btnSubstract === true || btnAdd === true) {
-    const containerOfLiItem = e.target.parentNode.parentNode;
-    const clAmountItemsInCart = containerOfLiItem.querySelector('.clHildebrando');
+    const containerOfLiItem = e.target.parentNode.parentNode.parentNode;
+    const clAmountItemsInCart = containerOfLiItem.querySelector('.clTheSpanWithAmountItems');  // TODO: cambiar el nom,bre de esta clase
     let nAmountItems = Number(clAmountItemsInCart.innerHTML);
     if (btnSubstract) {
       nAmountItems > 0 ? nAmountItems -= 1 : 0;
@@ -49,30 +49,40 @@ function fnPaintCart(oneList) {
   oneList.forEach(element => {
     const { id, name, url_image, unit } = element;
     ul.innerHTML += `
+
     <li class="list-group-item">
-        <div class="container-fluid align-items-center row">
-            <div class= "col-12 col-sm-3 col-md-4 p-0 m-0">
-                <span class="lead">${name}</span>
-            </div>
-            <div class= "col-12 col-sm-1 col-md-2 p-0 m-0">
-                <img src=${url_image} width="50px"></img>
-            </div>
-            <div class="col-12 col-sm-4 col-md-1 col-lg-1">
-               <p class="fs-4 fw-bold ms-3 text-center pt-3 clHildebrando">1</p>
-            </div>
-            <div class= "col-12 col-sm-2 col-md-2">
-                <button id=${id} class="btn btn-dark fs-5 btm-sm float-end clBtnAddOneLess">Menos</button>
-            </div>
-            <div class= "col-12 col-sm-2 col-md-2">
-                <button id=${id} class="btn btn-dark fs-5 btm-sm float-end clBtnAddOneMore">Más</button>
-            </div>
-        </div>
-    </li>
+    <div id=${id} class="container-fluid row justify-content-center align-items-center">
+      <div class="col-12 col-sm-4 col-md-3 col-lg-3 p-0 m-0">
+        <img src="images/cart-empty03.png" width="50px"></img>
+      </div>
+      <div class="col-12 col-sm-4 col-md-3 col-lg-3 p-0 m-0">
+        <span class="lead">${name}</span>
+        <p class="h5">$99,999</p>
+      </div>
+      <div class="col-12 col-sm-4 col-md-2 col-lg-2 p-0 m-0">
+        <span class="lead">x 3 Lts.</span>
+      </div>
+      <div class="col-12 col-sm-4 col-md-1 col-lg-1">
+      <span id="" class="bg-transparent btn-dark">
+        <img class="mx-4 clBtnAddOneLess" src="./images/substract01.png" width="25" height="25">
+      </span>
+      </div>
+      <div class="col-12 col-sm-4 col-md-1 col-lg-1">
+        <p class="fs-4 fw-bold ms-3 text-center pt-3 clTheSpanWithAmountItems">1</p>
+      </div>
+      <div class="col-12 col-sm-4 col-md-1 col-lg-1">
+      <span id="" class="bg-transparent btn-dark">
+        <img class="mx-4 clBtnAddOneMore" src="./images/add01.png" width="25" height="25">
+      </span>
+      </div>
+    </div>
+  </li>
+
     `
 
     const nn = `
-  <li id=${id}  class="list-group-item">
-  <div class="container-fluid row justify-content-center align-items-center">
+  <li class="list-group-item">
+  <div id=${id} class="container-fluid row justify-content-center align-items-center">
     <div class="col-12 col-sm-4 col-md-3 col-lg-3 p-0 m-0">
       <img src="images/cart-empty03.png" width="50px"></img>
     </div>
@@ -83,6 +93,7 @@ function fnPaintCart(oneList) {
     <div class="col-12 col-sm-4 col-md-2 col-lg-2 p-0 m-0">
       <span class="lead">x 3 Lts.</span>
     </div>
+
     <div class="col-12 col-sm-4 col-md-1 col-lg-1">
       <!-- <button id=${id} class="btn btn-dark fs-5 btm-sm float-end clBtnAddSubstractCart">Borrar</button> -->
       <button id="" class="bg-transparent btn-dark clBtnAddOneLess">
@@ -90,7 +101,7 @@ function fnPaintCart(oneList) {
       </button>
     </div>
     <div class="col-12 col-sm-4 col-md-1 col-lg-1">
-      <p id="idAmountToCart" class="fs-4 fw-bold ms-3 text-center pt-3 clHildebrando">1</p>
+      <p id="idAmountToCart" class="fs-4 fw-bold ms-3 text-center pt-3 clTheSpanWithAmountItems">1</p>
     </div>
     <div class="col-12 col-sm-4 col-md-1 col-lg-1">
       <button id="" class="bg-transparent clBtnAddOneMore">
