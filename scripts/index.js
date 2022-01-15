de23923228ea435c6404d3d8d68499a6d66e94f7
hildebrandosv <hildebrandosv@gmail.com>
2022-01-15 17:56:05 -0500

    Hice los calculos para mostrar los valores en cada línea LI donde sale cada producto, y también el total de productos diferentes y el valor a pagar en el botón de IR A PAGAR

--- scripts/index.js
+++ scripts/index.js
@@ -160,30 +160,40 @@ idBtnAddToCart_ModalProdOfferToCart.addEventListener('click', e => {
    idMsgItemAdded.classList.remove('d-none')
    setTimeout(() => {
       idMsgItemAdded.classList.add('d-none')
-   }, 3000);
+   }, 2000);
 })
 
 //╔════════════════════════════════════════════════╗
 //║             FUNCTION DEFINITION                ║
 //╚════════════════════════════════════════════════╝
- //
- // Show all products that contains the cart and show  the options to empty to cart or to go to pay.
- function fnPaintCart(oneList) {
-   idUlItemsCartList.innerHTML= "";  // Deletes the container with items and rewrite it
+//
+// Show all products that contains the cart and show  the options to empty to cart or to go to pay.
+function fnPaintCart(oneList) {
+   let nTotalToPay = 0;
+   const nDiferentItemsTotal = oneList.length;
+   const idDifferentItemsTotalInCartDetail = document.getElementById('idDifferentItemsTotalInCartDetail');
+   idDifferentItemsTotalInCartDetail.innerHTML = nDiferentItemsTotal;
+   idUlItemsCartList.innerHTML = "";  // Deletes the container with items and rewrite it
    oneList.forEach(element => {
-     const { id, name, url_image, unit } = element;
-     idUlItemsCartList.innerHTML += `           
+      const { id, name, url_image, unit, total_items, price, discount } = element;
+      // Calculates the totals
+      const idTotalToPayCartDetail = document.getElementById('idTotalToPayCartDetail');
+      const price_net = Math.round(price * ((100 - discount) / 100));
+      nTotalToPay = total_items * price_net;
+      idTotalToPayCartDetail.innerHTML = fnNumberFormat(nTotalToPay, '$');
+      // Paints each item whit its data line (a LI tag) per line (a LI tag) 
+      idUlItemsCartList.innerHTML += `           
                      <li class="list-group-item">
                        <div id=${id} class="container-fluid row justify-content-center align-items-center">
                          <div class="col-12 col-sm-4 col-md-3 col-lg-3 p-0 m-0">
-                           <img src="images/cart-empty03.png" width="50px"></img>
+                           <img src=${url_image} width="50px"></img>
                          </div>
                          <div class="col-12 col-sm-4 col-md-3 col-lg-3 p-0 m-0">
                            <span class="lead">${name}</span>
-                           <p class="h5">$99,999</p>
+                           <p class="h5">${fnNumberFormat(price_net, '$')}</p>
                          </div>
                          <div class="col-12 col-sm-4 col-md-2 col-lg-2 p-0 m-0">
-                           <span class="lead">x 3 Lts.</span>
+                           <span class="lead">x ${unit}</span>
                          </div>
                          <div class="col-12 col-sm-4 col-md-1 col-lg-1">
                            <span id="" class="bg-transparent btn-dark">
@@ -202,7 +212,7 @@ idBtnAddToCart_ModalProdOfferToCart.addEventListener('click', e => {
                      </li>
                    `
    });
- }
+}
 // GET promise to load ONE RECORD: Query only a product of the products in offer
 async function fnLoadOneProduct(urlData, idKey) {
    // Local variables for the item to be added to the
